66b93d02f5e589b48a0bb2d6af2be34cec5a9321
Dozie2001 <davidchidozie402@gmail.com>
2022-10-25 14:48:45 +0100

    1st day working on this

---
+++ settings.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+class Settings:
+    """A class that stores all the settings for the Alien Invasion program"""
+    def __init__(self):
+        self.width = 1200
+        self.height = 800
+        self.bg_color = (0, 255, 0)
+
+        #Ship settings:
+        self.ship_settings = 1.5
+        
+
+
+    
+
---
+++ ship.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import pygame
+
+class Ship:
+    """A class to manage the ship behavior"""
+    def __init__(self, ai_game):
+        """Initialize the ship and set its starting position."""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        #Load Image and get it's rect
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        #Start eaach ship at the bottom of the screen
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        #Movement Flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ships position based on movement flag"""
+        # Update the ship's x value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.rect.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
+
+    def blitme(self):
+        """Draw the ship and its current loaction"""
+        self.screen.blit(self.image, self.rect)
+
+    
